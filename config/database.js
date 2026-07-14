585e7c70e0c3eaa9fa3ec25b98c62dbb81f0fed6
kishan <kishan.chauhan@markupmail.com>
2026-07-14 17:06:44 +0530

    change db

--- config/database.js
+++ config/database.js
@@ -3,10 +3,7 @@ require("dotenv").config();
 
 const dbConnect = () => {
   mongoose
-    .connect(process.env.DATABASE_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.DATABASE_URL)
     .then(() => console.log("✅ Database connection successful"))
     .catch((error) => {
       console.error("❌ Database connection failed:", error.message);
